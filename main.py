6978b15ccc0d11fda025a091fc8608b5d25bff0f
Alex Man <alexman@MacBook-Pro.local>
2020-08-07 10:30:52 -0700

    mademuchbetter

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def myapp():
-    return '<h1>hi</h1>'
+    value = 0
+    if request.method == 'POST':
+        value = int(request.form['num'])
+        value *= 5.5
+    
+    return render_template('index.html', val=value)
 
 if __name__ == "__main__":
     app.run(debug=False)
