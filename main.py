98e0a75d882bd00cac437b0fc88085787fe7183d
Alex Man <alexman@MacBook-Pro.local>
2020-09-01 16:07:02 -0700

    even better

--- main.py
+++ main.py
@@ -1,19 +1,56 @@
-from flask import Flask, render_template, request, url_for, redirect
+# from flask import Flask, render_template, request, url_for, redirect
+# import smtplib
+
+
+# app = Flask(__name__)
+
+# @app.route('/', methods=['POST', 'GET'])
+# def myapp():
+#     value = 0
+#     if request.method == 'POST':
+#         value = int(request.form['num'])
+#         value *= 5.5
+#         print('poooo')
+#     if request.method == "GET":
+#         print('ppoooo')    
+#     return render_template('index.html', val=value)
+
+# if __name__ == "__main__":
+#     app.run(debug=False)
+
+from flask import Flask, request, redirect, url_for, render_template
 import smtplib
 
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
-def myapp():
-    value = 0
-    if request.method == 'POST':
-        value = int(request.form['num'])
-        value *= 5.5
-        print('poooo')
-    if request.method == "GET":
-        print('ppoooo')    
-    return render_template('index.html', val=value)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/suggestions', methods = ["POST", "GET"])
+def suggestions():
+    if request.method == "POST":
+        stuff = request.form["sug"]
+        print(stuff)
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
+
+            smtp.login('suggestabj@gmail.com', 'P@@ssword123')
+
+            subject = "suggestion"
+            body = str(stuff)
+
+            msg = f"Subject: {subject}\n\n{body}"
+            smtp.sendmail('suggestabj@gmail.com', 'agentbobjelly@gmail.com', msg)
+    return render_template("suggestions.html")
+
 
 if __name__ == "__main__":
     app.run(debug=False)
